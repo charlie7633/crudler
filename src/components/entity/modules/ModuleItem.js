7579f26b18aa6f88e98bc8357706ff407fa0db2e
Charles Ogunsanya <83789291+charlie7633@users.noreply.github.com>
2026-03-26 20:16:55 +0000

    file structure change
    asked a friend for help on file structure might need to ask lecturer.

---
+++ src/components/entity/modules/ModuleItem.js
@@ -0,0 +1,27 @@
+// src/components/entity/modules/ModuleItem.js
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+const ModuleItem = ({ module, onSelect }) => {
+    return (
+        <Pressable onPress={() => onSelect(module)}>
+            <View style={styles.item}>
+                <Text style={styles.text}>
+                    {module.ModuleCode} {module.ModuleName}
+                </Text>
+            </View>
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    item: {
+        paddingVertical: 15,
+        borderTopWidth: 1,
+        borderColor: 'lightgrey',
+    },
+    text: {
+        fontSize: 16,
+    },
+});
+
+export default ModuleItem;
