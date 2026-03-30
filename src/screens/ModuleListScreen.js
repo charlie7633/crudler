a903899f4d4242ba315515a6ee9a5cd4706aa7ff
Charles Ogunsanya <83789291+charlie7633@users.noreply.github.com>
2026-03-30 12:35:54 +0100

    added the ability to delete tabs

--- src/screens/ModuleListScreen.js
+++ src/screens/ModuleListScreen.js
@@ -1,4 +1,5 @@
 // src/screens/ModuleListScreen.js
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Screen from '../components/layout/Screen';
 import ModuleList from '../components/entity/modules/ModuleList';
@@ -6,18 +7,19 @@ import initialModules from '../data/modules';
 
 const ModuleListScreen = () => {
     // Initialisations ---------------------
-    const modules = initialModules;
-
     // State -------------------------------
+    const [modules, setModules] = useState(initialModules);
+
     // Handlers ----------------------------
-    const handleSelect = (module) => {
-        alert(`Item ${module.ModuleCode} selected`);
+    const handleDelete = (moduleToDelete) => {
+        // This creates a brand new array keeping only the modules that DON'T match the one we clicked
+        setModules(modules.filter((module) => module.ModuleCode !== moduleToDelete.ModuleCode));
     };
 
     // View --------------------------------
     return (
         <Screen>
-            <ModuleList modules={modules} onSelect={handleSelect} />
+            <ModuleList modules={modules} onSelect={handleDelete} />
         </Screen>
     );
 };
